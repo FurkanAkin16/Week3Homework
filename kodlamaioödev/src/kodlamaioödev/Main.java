18a1351bf6b3bf7a0289a844cf738042373e455a
Furkan Akın <118662756+FurkanAkin16@users.noreply.github.com>
2022-12-05 16:46:46 +0300

    Add files via upload

---
+++ kodlamaioödev/src/kodlamaioödev/Main.java
@@ -0,0 +1,13 @@
+package kodlamaioödev;
+
+import enitity.Catagory;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Catagory catagory = new Catagory();
+		catagory.getCategoryName();
+		System.out.println(catagory.setCategoryName(null)));
+	}
+
+}
